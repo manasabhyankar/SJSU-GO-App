9acf7ea0ad77417d3018e0b1ad9929be34c061a2
manasabhyankar <manasthemenace@gmail.com>
2019-06-27 00:49:20 -0700

    Following Kotlin Android Studio guide at: https://codelabs.developers.google.com/codelabs/build-your-first-android-app-kotlin/#0
    Finished tutorial up to page 7

--- app/src/main/java/com/example/sjsugo/MainActivity.kt
+++ app/src/main/java/com/example/sjsugo/MainActivity.kt
@@ -2,12 +2,18 @@ package com.example.sjsugo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+    fun toastMe(view: View){
+        val myToast = Toast.makeText(this, "Toast!", Toast.LENGTH_SHORT)
+        myToast.show()
     }
 }
 //this is a test comment to see if GitHub vcs is working
