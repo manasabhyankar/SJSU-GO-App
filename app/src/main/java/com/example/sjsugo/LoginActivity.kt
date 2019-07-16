d6ccafce87dda1584869987e7b663e5d5aea59dd
Manas Abhyankar <manasthemenace@gmail.com>
2019-07-15 23:18:06 -0700

    Added Firebase integration for: -Login -Sign-up -Detect if user is already signed in (let's call it user persistence)
    Changed onClicks to OnClickListeners
    Renamed module IDs to match a singular formatting
    Changed formatting on LoginActivity and SignUpActivity

--- app/src/main/java/com/example/sjsugo/LoginActivity.kt
+++ app/src/main/java/com/example/sjsugo/LoginActivity.kt
@@ -3,30 +3,67 @@ package com.example.sjsugo
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
-import android.widget.Spinner
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
-
+    private lateinit var firebaseAuth : FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        firebaseAuth = FirebaseAuth.getInstance()
     }
 
+    public override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = firebaseAuth.currentUser
+        updateUI(currentUser)
+    }
 
-    fun signupClick (view: View){
-        //create an intent to start the second activity
-        val signupIntent = Intent(this, SignUpActivity::class.java)
-
-        //start the new activity
-        startActivity(signupIntent)
+    fun updateUI(user : FirebaseUser?){
+        //TODO: Add user details to dashboard activity based on login status
+        startActivity(Intent(this, DashboardActivity::class.java))
+    }
+    fun signUpClick (view: View){
+        //redirect to SignUpActivity
+        direct_to_signup.setOnClickListener {
+            startActivity(Intent(this, SignUpActivity::class.java))
+            finish()
+        }
     }
 
     fun loginClick (view: View){
-        //create an intent to start the second activity
-        val loginIntent = Intent(this, DashboardActivity::class.java)
-
-        //start the new activity
-        startActivity(loginIntent)
+        //set onClickListener to call the doSignIn method
+        log_in_button.setOnClickListener {
+            doSignIn()
+        }
     }
+    private fun doSignIn(){
+        if((login_email.text.isNullOrEmpty() or login_pass.text.isNullOrEmpty())){
+            Toast.makeText(this, "Please enter valid credentials.", Toast.LENGTH_SHORT).show()
+        }
+        else{
+            firebaseAuth.signInWithEmailAndPassword(login_email.text.toString(), login_pass.text.toString())
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        Log.d("Success", "worked")
+                        Toast.makeText(this, "Successfully logged in. Welcome!", Toast.LENGTH_SHORT).show()
+                        // Sign in success, update UI with the signed-in user's information
+                        val user = firebaseAuth.currentUser
+                        startActivity(Intent(this, DashboardActivity::class.java))
+
+                    } else {
+                        Log.d("Failure", "didn't work")
+                        Toast.makeText(this, "Sign in failed! Please check your username and password.", Toast.LENGTH_SHORT).show()
+                        updateUI(null)
+                    }
+                }
+        }
+    }
+
 }
--- app/src/main/java/com/example/sjsugo/SignUpActivity.kt
+++ app/src/main/java/com/example/sjsugo/SignUpActivity.kt
@@ -6,18 +6,22 @@ import android.os.Bundle
 import android.view.View
 import android.widget.ArrayAdapter
 import android.widget.Spinner
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import kotlinx.android.synthetic.main.activity_sign_up.*
 
 class SignUpActivity : AppCompatActivity() {
-
+    private lateinit var firebaseAuth : FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        firebaseAuth = FirebaseAuth.getInstance()
         setContentView(R.layout.activity_sign_up)
-
-        val spinner: Spinner = findViewById(R.id.MajorDropdown)
+        val spinner: Spinner = findViewById(R.id.department_spinner)
         // Create an ArrayAdapter using the string array and default spinner layout
         ArrayAdapter.createFromResource(
             this,
-            R.array.departmentMajor,
+            R.array.department_major,
             android.R.layout.simple_spinner_item
         ).also {adapter ->
             // Specify the layout to use when the list of choices appears
@@ -45,18 +49,53 @@ class SignUpActivity : AppCompatActivity() {
     }
 
     fun cancelClick(view: View) {
-        //create an Intent to go back to Log In activity
-        val cancelIntent = Intent(this, LoginActivity::class.java)
-
-        //Going back to the start activity
-        startActivity(cancelIntent)
+        //using onClickListener to trigger activity changes
+        cancel_button.setOnClickListener{
+            startActivity(Intent(this, LoginActivity::class.java))
+        }
     }
 
-    fun signupClick(view: View) {
-        //create an Intent to go back to Log In activity
-        val signupIntent = Intent(this, DashboardActivity::class.java)
+    fun updateUI(user : FirebaseUser?){
+        //TODO: Add user details to dashboard activity based on login status
+    }
 
-        //Going back to the start activity
-        startActivity(signupIntent)
+    fun signUpClick(view: View) {
+
+        signup_button.setOnClickListener {
+            doSignUp()
+        }
+    }
+    private fun doSignUp(){
+        if(signup_id.text.toString().isEmpty()){
+            Toast.makeText(this, "Please enter a valid SJSU ID.", Toast.LENGTH_SHORT).show()
+        }
+        if(signup_id.text.toString().isEmpty()){
+            Toast.makeText(this, "Please enter a valid email.", Toast.LENGTH_SHORT).show()
+        }
+        if(signup_pass.text.toString().isEmpty()){
+            Toast.makeText(this, "Please enter a valid password.", Toast.LENGTH_SHORT).show()
+        }
+        if(first_name_text.text.toString().isEmpty()){
+            Toast.makeText(this, "Please enter a first name.", Toast.LENGTH_SHORT).show()
+        }
+        if(last_name_text.toString().isEmpty()){
+            Toast.makeText(this, "Please enter a last name.", Toast.LENGTH_SHORT).show()
+        }
+
+        else{
+            firebaseAuth.createUserWithEmailAndPassword(signup_id.text.toString(), signup_pass.text.toString())
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        Toast.makeText(this, "Successfully created account. Welcome!", Toast.LENGTH_SHORT).show()
+                        // Sign in success, update UI with the signed-in user's information
+                        val user = firebaseAuth.currentUser
+                        startActivity(Intent(this, DashboardActivity::class.java))
+
+                    } else {
+                        Toast.makeText(this, "Sign up failed! Please try again later.", Toast.LENGTH_SHORT).show()
+                        updateUI(null)
+                    }
+                }
+        }
     }
 }
